9ad83a0eb8f31612cb17848f2198cd15bfab5166
John <117133884+spurling5575@users.noreply.github.com>
2022-11-02 10:09:55 +0900

    [fix] code

--- passCheck.py
+++ passCheck.py
@@ -1,8 +1,12 @@
-admin_pswd = 'shiitake'
+import hashlib
+
+admin_hs = "70fdc61e86c7b376d488ae331f093c9676158cbe80d7d77ed38c98a130a84595"
+
 print('パスワードを入力してください')
 pswd = input()
+hs = hashlib.sha256(pswd.encode()).hexdigest()
 
-if admin_pswd == pswd:
+if admin_hs == hs:
     print('ようこそ！')
 else:
     print('パスワードが違います')
